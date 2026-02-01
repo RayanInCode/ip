ba6e7f16558a17f3165c43aee8f4208cdbb043e0
RayanInCode <rayanrahman4017@gmail.com>
2026-02-01 18:34:45 +0800

    Level-0: Rename chatbot and add greeting

---
+++ src/main/java/Magnemite.java
@@ -0,0 +1,11 @@
+public class Magnemite {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Magnemite");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
