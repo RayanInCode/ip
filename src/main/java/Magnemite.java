73d31c55596df8ba1e15cb22217fbc0cc057dd25
RayanInCode <rayanrahman4017@gmail.com>
2026-02-02 17:16:37 +0800

    Listing feature added

--- src/main/java/Magnemite.java
+++ src/main/java/Magnemite.java
@@ -7,12 +7,26 @@ public class Magnemite {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         String line;
+        String[] tasks = new String[100];
+        int counter = 0;
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < counter; i++) {
+                    System.out.printf("%d. ", i+1);
+                    System.out.println(tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + line);
+                System.out.println("____________________________________________________________");
+                tasks[counter] = line;
+                counter++;
+            }
         } while (!line.equals("bye"));
         System.out.println("Bye. Hope to see you again!");
         System.out.println("____________________________________________________________");
