b3e521d850500a386724d3f5bebc6962b87385ba
RayanInCode <rayanrahman4017@gmail.com>
2026-02-22 03:14:05 +0800

    Added events, todos, deadlines to be able to set by date

--- src/main/java/Magnemite.java
+++ src/main/java/Magnemite.java
@@ -2,26 +2,6 @@ import java.util.Scanner;
 
 public class Magnemite {
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-        protected String tag; // T = todo, D = deadline, E = event
-
-        public Task(String description, String tag) {
-            this.description = description;
-            this.tag = tag;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " ");
-        }
-
-        public String getTagIcon() {
-            return tag;
-        }
-    }
-
     public static void main(String[] args) {
 
         System.out.println("____________________________________________________________");
@@ -31,7 +11,6 @@ public class Magnemite {
 
         Scanner in = new Scanner(System.in);
         String line;
-        String command;
 
         Task[] tasks = new Task[100];
         int counter = 0;
@@ -39,76 +18,78 @@ public class Magnemite {
 
         do {
             line = in.nextLine();
-            String[] parts = line.split(" ");
-            command = parts[0];
 
-            if (command.equals("list")) {
+            if (line.startsWith("list")) {
 
                 System.out.println("____________________________________________________________");
                 for (int i = 0; i < counter; i++) {
                     System.out.printf("%d. ", i + 1);
-                    System.out.println(
-                            "[" + tasks[i].getTagIcon() + "] "
-                                    + "[" + tasks[i].getStatusIcon() + "] "
-                                    + tasks[i].description
-                    );
+                    System.out.println(tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
 
-            } else if (command.equals("mark")) {
+            } else if (line.startsWith("mark")) {
+                String[] parts = line.split(" ");
                 num = Integer.parseInt(parts[1]);
                 tasks[num - 1].isDone = true;
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(
-                        "[" + tasks[num - 1].getTagIcon() + "] "
-                                + "[" + tasks[num - 1].getStatusIcon() + "] "
-                                + tasks[num - 1].description
-                );
+                System.out.println(tasks[num - 1]);
 
-            } else if (command.equals("unmark")) {
-
+            } else if (line.startsWith("unmark")) {
+                String[] parts = line.split(" ");
                 num = Integer.parseInt(parts[1]);
                 tasks[num - 1].isDone = false;
                 System.out.println("Ok! I've marked this task as not yet done:");
-                System.out.println(
-                        "[" + tasks[num - 1].getTagIcon() + "] "
-                                + "[" + tasks[num - 1].getStatusIcon() + "] "
-                                + tasks[num - 1].description
-                );
+                System.out.println(tasks[num - 1]);
 
-            } else if (command.equals("todo")) {
+            } else if (line.startsWith("todo")) {
 
                 String taskDesc = line.substring(5);
-                tasks[counter] = new Task(taskDesc, "T");
-                counter++;
+                tasks[counter] = new Todo(taskDesc);
+
                 System.out.println("____________________________________________________________");
                 System.out.println("Added a todo:");
-                System.out.println("[T] [ ] " + taskDesc);
+                System.out.println(tasks[counter]);
+                counter++;
+                System.out.println(" Now you have " + counter + " tasks in the list.");
                 System.out.println("____________________________________________________________");
 
-            } else if (command.equals("deadline")) {
+            } else if (line.startsWith("deadline")) {
 
                 String taskDesc = line.substring(9);
-                tasks[counter] = new Task(taskDesc, "D");
-                counter++;
+                String[] parts = taskDesc.split(" /by ");
+                String description = parts[0];
+                String by = parts[1];
+
+                tasks[counter] = new Deadline(description, by);
+
                 System.out.println("____________________________________________________________");
                 System.out.println("Added a deadline:");
-                System.out.println("[D] [ ] " + taskDesc);
+                System.out.println(tasks[counter]);
+                counter++;
+                System.out.println(" Now you have " + counter + " tasks in the list.");
                 System.out.println("____________________________________________________________");
 
-            } else if (command.equals("event")) {
+            } else if (line.startsWith("event")) {
 
                 String taskDesc = line.substring(6);
-                tasks[counter] = new Task(taskDesc, "E");
-                counter++;
+                String[] parts = taskDesc.split(" /");
+                String description = parts[0];
+                String start = parts[1].substring(4);
+                String end = parts[2].substring(2);
+
+                tasks[counter] = new Event(description, start, end);
                 System.out.println("____________________________________________________________");
                 System.out.println("Added an event:");
-                System.out.println("[E] [ ] " + taskDesc);
+                System.out.println(tasks[counter]);
+                counter++;
+
+                System.out.println(" Now you have " + counter + " tasks in the list.");
                 System.out.println("____________________________________________________________");
 
             }
 
-        } while (!line.equals("bye"));
+        } while (!line.startsWith("bye"));
 
         System.out.println("Bye. Hope to see you again!");
         System.out.println("____________________________________________________________");
